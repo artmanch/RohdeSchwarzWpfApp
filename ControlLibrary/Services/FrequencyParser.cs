f0d2a15337dd3f7d32249463f6331a142efe185b
Artem B <artembritvin@gmail.com>
2021-09-02 21:02:07 +0300

    Заготовка сервиса парсинга строки

---
+++ ControlLibrary/Services/FrequencyParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ControlLibrary.Services.Interfaces;
+
+namespace ControlLibrary.Services
+{
+    public class FrequencyParser : IParser
+    {
+        public (long frequency, Metrics metric) Parse(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ControlLibrary/Services/Interfaces/IParser.cs
@@ -0,0 +1,14 @@
+﻿namespace ControlLibrary.Services.Interfaces
+{
+    public interface IParser
+    {
+        public (long frequency, Metrics metric) Parse(string value);
+    }
+
+    public enum Metrics
+    {
+        Hz,
+        kHz,
+        MHz
+    }
+}
