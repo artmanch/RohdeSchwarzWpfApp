2fb1d564304de928e43b036306591ca8663c0f8c
Artem B <artembritvin@gmail.com>
2021-09-04 23:15:09 +0300

    Добавлена логика получения частоты в Гц и формирования значения частоты в TextBox в нужных единицах измерения

--- ControlLibrary/Controls/FrequencyControl.xaml.cs
+++ ControlLibrary/Controls/FrequencyControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using ControlLibrary.Services;
@@ -11,30 +13,92 @@ namespace ControlLibrary.Controls
     /// </summary>
     public partial class FrequencyControl : UserControl
     {
+        private IParser Parser { get; set; }
+
+        public FrequencyControl()
+        {
+            Parser = new FrequencyParser();
+            InitializeComponent();
+        }
+
         /// <summary>
-        ///Поле для хранения частоты в Гц (Hz)
+        ///Свойство зависимости для хранения частоты в Гц (Hz)
         /// </summary>
-        public double Frequency { get; set; } = 0;
+        public static readonly DependencyProperty FrequencyProperty =
+            DependencyProperty.Register(
+                nameof(Frequency),
+                typeof(double),
+                typeof(FrequencyControl),
+                new PropertyMetadata(default(double)));
 
-        private IParser Parser { get; set; } = new FrequencyParser();
+        [Description("Частота в Гц")]
+        public double Frequency
+        {
+            get => (double) GetValue(FrequencyProperty);
+            set => SetValue(FrequencyProperty, value);
+        }
 
-        public FrequencyControl() => InitializeComponent();
-
-        public string Text 
+        /// <summary>
+        /// Свойство для работы с текстом TextBox-а
+        /// </summary>
+        public string Text
         {
             get => FrequencyTextBox.Text;
             set => FrequencyTextBox.Text = value;
         }
 
-        //todo:добавить логику получения значения поля Frequency в Гц и формирования Text 
         private void FrequencyConfirmed(object sender, KeyEventArgs e)
         {
             if(e.Key == Key.Enter)
             {
                 var textBox = sender as TextBox;
                 if(sender is null) return;
-                
+
                 var value = Parser.Parse(Text);
+                Frequency = GetFrequency(value.frequency, value.metric);
+                Text = GetText(value.frequency, value.metric);
+            }
+        }
+
+        /// <summary>
+        /// Получаем частоту в Гц 
+        /// </summary>
+        /// <param name="frequency">частота в текущих е.и.</param>
+        /// <param name="metric">е.и.</param>
+        /// <returns>частота в Гц</returns>
+        private double GetFrequency(double frequency, Metrics metric) => metric switch
+        {
+            Metrics.kHz => frequency * 1000,
+            Metrics.MHz => frequency * 1000000,
+            _ => frequency
+        };
+
+        /// <summary>
+        /// Получаем строковое представление частоты с учетом е.и.
+        /// </summary>
+        /// <param name="frequency">частота в текущих е.и.</param>
+        /// <param name="metric">е.и.</param>
+        /// <returns>строковое представление частоты с указанием е.и.)</returns>
+        private string GetText(double frequency, Metrics metric)
+        {
+            switch(metric)
+            {
+                case Metrics.Hz:
+                    if(frequency < 1000) return $"{frequency:f4} Гц";
+                    else if(frequency > 1000 && frequency < 1000000) return $"{frequency / 1000:f4} кГц";
+                    else return $"{frequency / 1000000:f4} МГц";
+
+                case Metrics.kHz:
+                    if(frequency < 1000 && frequency > 1) return $"{frequency:f4} кГц";
+                    else if(frequency > 1000) return $"{frequency / 1000:f4} МГц";
+                    else return $"{frequency * 1000:f4} Гц";
+
+                case Metrics.MHz:
+                    if(frequency > 1) return $"{frequency:f4} МГц";
+                    else if(frequency < 1 && frequency > 0.001) return $"{frequency * 1000:f4} кГц";
+                    else return $"{frequency * 1000000:f4} Гц";
+
+                default: throw new InvalidOperationException("Не существующая операция");
             }
         }
     }
