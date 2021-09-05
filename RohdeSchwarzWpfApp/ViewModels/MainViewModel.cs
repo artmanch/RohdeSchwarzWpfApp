f97288d39b38c7be63dd059332f05d8d57cd0cfa
Artem B <artembritvin@gmail.com>
2021-09-05 12:55:55 +0300

    Пример привязки ViewModel-и к свойству Frequency

---
+++ RohdeSchwarzWpfApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace RohdeSchwarzWpfApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propetyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propetyName));
+        }
+
+        private double _Frequency;
+        public double Frequency
+        {
+            get => _Frequency;
+            set
+            {
+                if(Equals(_Frequency, value)) return;
+
+                _Frequency = value;
+                Debug.WriteLine($"Frequency ViewModel Value = {_Frequency}");
+                OnPropertyChanged();
+            }
+        }
+
+        private string _Title = "Test";
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if(Equals(_Title, value)) return;
+
+                _Title = value;
+                Debug.WriteLine("Title ViewModel");
+                OnPropertyChanged();
+            }
+        }
+    }
+}
